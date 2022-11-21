3956394c5d2004d76a1191c7a41c40795ac3ac56
YesunPark <lynn08082@gmail.com>
2022-11-21 23:52:06 +0900

    Add: 할일 클릭 시 체크박스색, 글자색 변하는 기능 완성

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BoxStyle from './Box.Style';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
+import styled from 'styled-components';
 
 function List() {
+  const [checked, setChecked] = useState(false);
+  function clickCheckBox() {
+    setChecked(!checked);
+  }
+
   return (
-    <BoxStyle className="list">
-      <div>
-        <FontAwesomeIcon icon={faSquareCheck} className="icon" />
-        <div>설거지하기</div>
-      </div>
-    </BoxStyle>
+    <ListContainer id={checked.toString()}>
+      <BoxStyle className="list">
+        <div className="checkbox" onClick={clickCheckBox}>
+          <FontAwesomeIcon icon={faSquareCheck} className="icon" />
+          <div className="content">설거지하기</div>
+        </div>
+      </BoxStyle>
+    </ListContainer>
   );
 }
+
+const ListContainer = styled.div`
+  .icon {
+    color: ${(props) => (props.id === 'false' ? props.theme.color.main : 'green')};
+  }
+  .content {
+    color: ${(props) => (props.id === 'false' ? props.theme.color.txt : props.theme.color.grayTxt)};
+  }
+`;
 
 export default List;
