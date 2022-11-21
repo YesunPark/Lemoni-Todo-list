6bb30f963698d036e53531992dabffcac29ad3c4
YesunPark <lynn08082@gmail.com>
2022-11-21 11:29:53 +0900

    Add: styled-reset install, 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import styled from 'styled-components'
 
 function App() {
-  return <></>;
+  return <><TitleContainer></TitleContainer></>;
 }
+
+const TitleContainer = styled.div`
+  
+`
 
 export default App;
