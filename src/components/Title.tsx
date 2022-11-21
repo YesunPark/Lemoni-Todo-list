a45a3a1d922bfecd55d5a2325670e458bd1ec810
YesunPark <lynn08082@gmail.com>
2022-11-21 14:53:21 +0900

    Add: fontawesome install, add lemon icon

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLemon } from '@fortawesome/free-solid-svg-icons';
 
 function Title() {
-  return <TitleContainer></TitleContainer>;
+  return (
+    <TitleContainer>
+      <FontAwesomeIcon icon={faLemon} className="lemon-icon" />
+    </TitleContainer>
+  );
 }
 
 const TitleContainer = styled.div`
   height: 65px;
   background-color: ${(props) => props.theme.color.mainBg};
+  .lemon-icon {
+    color: ${(props) => props.theme.color.main};
+  }
 `;
 
 export default Title;
