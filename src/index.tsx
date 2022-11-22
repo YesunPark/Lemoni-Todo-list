d7ba863d69c1ac670cc92f26e06446a31442dc04
YesunPark <lynn08082@gmail.com>
2022-11-22 17:22:15 +0900

    Add: Recoil 설치, atom 파일 생성

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from 'assets/styles/global-styles';
+import { RecoilRoot } from 'recoil';
 import { ThemeProvider } from 'styled-components';
-import { theme } from 'assets/styles/theme';
 import App from './App';
+import { theme } from 'assets/styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </ThemeProvider>
   </React.StrictMode>,
 );
