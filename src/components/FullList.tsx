c1eb305b1c605f277f20aabd24d43dda87c50240
YesunPark <lynn08082@gmail.com>
2022-11-22 20:02:06 +0900

    Fix: 할일 목록이 늘어나면 스크롤기능 생기도록 수정

--- src/components/FullList.tsx
+++ src/components/FullList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { toDoArrState } from '../atom'; // atom으로 만든 전역상태
 import { useRecoilState } from 'recoil'; // 훅 import
 import EmptyList from './EmptyList';
@@ -8,13 +9,18 @@ function FullList() {
   const [listArr] = useRecoilState(toDoArrState);
 
   return (
-    <>
+    <FullListContainer>
       {!listArr.length && <EmptyList />}
       {listArr.map((list) => {
         return <ListItem content={list.content} key={list.idx} idx={list.idx} />;
       })}
-    </>
+    </FullListContainer>
   );
 }
+
+const FullListContainer = styled.div`
+  height: 520px;
+  overflow: scroll;
+`;
 
 export default FullList;
