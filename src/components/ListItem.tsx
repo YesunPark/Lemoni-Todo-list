390e721efb69321ef6389fff5f38a08e26839903
YesunPark <lynn08082@gmail.com>
2022-11-22 17:15:35 +0900

    Delete: 사용하지 않는 import 삭제

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import BoxStyle from './Box.Style';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareCheck } from '@fortawesome/free-regular-svg-icons';
