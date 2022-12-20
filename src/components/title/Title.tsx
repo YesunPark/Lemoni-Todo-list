7a5b3ca14146ba61ba080b5ecb8d6dd318574cfc
YesunPark <lynn08082@gmail.com>
2022-12-20 15:24:13 +0900

    Add: 달력에 오늘날짜 반영, 클릭 시 날짜 선택 기능 추가

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLemon } from '@fortawesome/free-solid-svg-icons';
 import BoxStyle from '../styles/Box.Style';
 import { theme } from '../../assets/styles/theme';
+import styled from 'styled-components';
 
 function Title() {
+  const today = `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`;
+  const [pickedDate, setPickedDate] = useState(today);
+  const handleDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPickedDate(e.target.value);
+  };
+
   return (
     <BoxStyle bgColor={theme.color.mainBg} className="title">
       <div>
         <FontAwesomeIcon icon={faLemon} className="icon" />
         <div>Lemoni To Do</div>
       </div>
+      <DateInput type="date" value={pickedDate} onChange={handleDate} />
     </BoxStyle>
   );
 }
+
+const DateInput = styled.input`
+  max-width: 110px;
+  margin-right: 10px;
+  font-size: 15px;
+  ::-webkit-calendar-picker-indicator {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
 
 export default Title;
